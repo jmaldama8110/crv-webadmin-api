6cdba7a6a195527429728d0eff6ab2dbe6bd1426
SolorzanoLuis <wolfsolorzano2000@gmail.com>
2022-12-12 02:47:10 -0600

    Servicio de suscrpción y solicitud de crédito por email

--- src/routers/emailsWebSite.js
+++ src/routers/emailsWebSite.js
@@ -5,13 +5,48 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const moment = require('moment');
 moment.locale('en');
 
+
+router.post('/subscribe', async(req, res) => {
+    try{
+
+        const data =  req.body;
+        const hoy = moment().format('L');
+        const hora = moment().format('LT');
+
+        // console.log(data);
+
+        const text = `
+                        <h3> <b>Se ha recibido un nuevo suscriptor para el boletín de noticias</b><p/></h3>
+
+                        <h4>
+                            Fecha de recepción: ${hoy} a las ${hora.toLowerCase()}<br/>
+                            Email: ${data.email} <br/>
+                        </h4>
+                    `;
+
+        sgMail.send({
+            to: 'callcenter@grupoconserva.mx',
+            // to: 'solorzanojluis01@gmail.com',
+            from: 'Servicio de Atención a Clientes<contacto@grupoconserva.mx>',
+            subject: 'Alerta CSV - Nuevo suscriptor',
+            html: text
+        });
+
+        res.status(201).send('Correo enviado!');
+
+    } catch(err){
+        console.log(err);
+        res.status(400).send(err);
+    }
+
+});
+
 router.post('/sendEmailHelp', async(req, res) => {
     try{
 
         const data =  req.body;
         const hoy = moment().format('L');
         const hora = moment().format('LT');
-        // console.log(data);
         const clienta = data.checked ? 'Sí' : 'No';
 
         let subjectOption = '';
@@ -32,10 +67,14 @@ router.post('/sendEmailHelp', async(req, res) => {
             pOption = 'Con el siguiente comentario:'
         }
         if(parseInt(data.option) === 3){
-            subjectOption = 'Nuevo queja';
+            subjectOption = 'Nueva queja';
             titleOption = 'Nueva queja recibida';
             txtOption = 'Se ha recibido una nueva queja';
             pOption = 'Con la siguiente queja:'
+        }
+
+        if(!data.email || data.email === undefined){
+            data.email = 'No se cuenta con una dirección de correo electrónico';
         }
 
         // <h2><b>Alerta CSV - ${titleOption}</b></h2><p/>
@@ -45,6 +84,11 @@ router.post('/sendEmailHelp', async(req, res) => {
         let text;
 
         if(data.checked){
+
+            if(!data.promotor || data.promotor === undefined){
+                data.promotor = 'No se cuenta con esta información'
+            }
+
             text = `
                         <h3> <b>${txtOption} con folio #1000</b><p/></h3>
 
@@ -56,7 +100,8 @@ router.post('/sendEmailHelp', async(req, res) => {
                             Clienta de Conserva: ${clienta} <br/>
                             Sucursal: ${data.branch} <br/>
                             Promotor: ${data.promotor} <br/>
-                            Estado de la república: ${data.province} <p/><br/>
+                            Estado de la república: ${data.province} <br/>
+                            Cuidad o Municipio: ${data.city} <p/>
                         </h4>
 
                         <h3><strong>${pOption}</strong><br/></h3>
@@ -72,7 +117,7 @@ router.post('/sendEmailHelp', async(req, res) => {
                             Nombre: ${data.name} <br/>
                             Email: ${data.email} <br/>
                             Teléfono: ${data.phone} <p/>
-                            Clienta de Conserva: ${clienta} <p/><br/>
+                            Clienta de Conserva: ${clienta} <p/>
 
                         </h4>
 
@@ -108,8 +153,6 @@ router.post('/sendEmailComplaint', async(req, res) => {
         const data =  req.body;
         const hoy = moment().format('L');
         const hora = moment().format('LT');
-        // console.log(data)
-        // const hoy = new Date();
         const anonimo = data.checked ? 'Sí' : 'No';
 
         const text = `
@@ -130,6 +173,7 @@ router.post('/sendEmailComplaint', async(req, res) => {
         const correos = [
             {
                 email: 'mmendoza@grupoconserva.mx'
+                // email: 'solorzanojluis01@gmail.com',
             },
             {
                 email: 'rgordillo@grupoconserva.mx'
@@ -162,8 +206,6 @@ router.post('/sendEmailRecruitment', async(req, res) => {
         const data =  req.body;
         const hoy = moment().format('L');
         const hora = moment().format('LT');
-        // const hoy = new Date();
-        // console.log(data);
 
         const text = `
                         <h3> <b>Se ha recibido una nueva Solicitud de empleo con folio #1000</b><p/></h3>
@@ -206,6 +248,63 @@ router.post('/sendEmailRecruitment', async(req, res) => {
     } catch(err){
         console.log(err);
         res.status(400).send(err)
+    }
+});
+
+
+router.post('/loan', async(req, res) => {
+    try{
+
+        const data = req.body;
+        const hoy = moment().format('L');
+        const hora = moment().format('LT');
+
+        const text = `
+                        <h3> <b>Se ha recibido una nueva Solicitud de empleo</b><p/></h3>
+
+                        <h4>
+                            Fecha de recepción: ${hoy} a las ${hora.toLowerCase()}<p/>
+
+                            Datos Personales <br/>
+                            Nombre: ${data.name} ${data.lastname} <br/>
+                            Estado: ${data.state} <br/>
+                            Municipio: ${data.country} <br/>
+                            Fecha de Nacimiento: ${data.birthday} <br/>
+                            Teléfono: ${data.phone} <br/>
+                            Teléfono Alterno: ${data.phoneAlternative} <p/>
+
+                            Datos del Negocio <br/>
+                            Giro al que pertenece: ${data.turn} <br/>
+                            Ubicación: ${data.ubication} <br/>
+                            Monto Solicitado: ${data.amount} <p/>
+
+                            Duración del negocio <br/>
+                            Años: ${data.bussineYear} año(s)<br/>
+                            Meses: ${data.bussineMoth} mes(es)<br/>
+                            Tipo de negocio: ${data.bussineType} <p/>
+
+                            Contacto <br/>
+                            Día disponible para contactar: ${data.day} <br/>
+                            Con horario de: ${data.timeFrom} hrs a ${data.timeTo} hrs<br/>
+                        </h4>
+
+                        <h3><strong>Comentarios:</strong></h3>
+                        <h4>${data.comment}</h4>
+                    `;
+
+        sgMail.send({
+            to: 'callcenter@grupoconserva.mx',
+            // to: 'solorzanojluis01@gmail.com',
+            from: 'Servicio de Atención a Clientes<contacto@grupoconserva.mx>',
+            subject: 'Alerta CSV - Solicitud de crédito recibida',
+            html: text
+        });
+
+        // res.status(201).send(data);
+        res.status(201).send('Correo enviado!');
+
+    } catch(err){
+        res.status(400).send(err);
     }
 });
 
