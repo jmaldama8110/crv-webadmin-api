6ec730c4132c35f04a1a06c6f23ef0f07a1a71dd
VICTOR3FRAIN <39277188+VICTOR3FRAIN@users.noreply.github.com>
2023-03-17 13:16:08 -0600

    Update docs.js
    soluccion del error
    
    "
    Failed to launch the browser process!
    [0317/190725.473682:ERROR:zygote_host_impl_linux.cc(90)] Running as root without --no-sandbox is not supported. See https://crbug.com/638180.
    TROUBLESHOOTING: https://pptr.dev/troubleshooting
    "

--- src/routers/docs.js
+++ src/routers/docs.js
@@ -22,7 +22,7 @@ router.get("/docs/html/account-statement", async (req, res) => {
 router.get("/docs/pdf/account-statement", async (req, res) => {
   try {
     // Create a browser instance
-    const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser'});
+    const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium-browser'},{args: ['--no-sandbox', '--disable-setuid-sandbox']});
     // Create a new page
     const page = await browser.newPage();
 
