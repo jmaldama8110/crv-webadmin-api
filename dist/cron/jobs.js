0f527346cbe35c8200c5e2c308bfbd6c0dd372e7
Josman <josemanuek@hotmail.com>
2024-04-30 15:48:14 -0600

    remove node-cron lib for temporary fix

--- dist/cron/jobs.js
+++ dist/cron/jobs.js
@@ -1,53 +1,17 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_cron_1 = __importDefault(require("node-cron"));
-const Nano = __importStar(require("nano"));
-let nano = Nano.default(`${process.env.COUCHDB_PROTOCOL}://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASS}@${process.env.COUCHDB_HOST}:${process.env.COUCHDB_PORT}`);
-node_cron_1.default.schedule('* 23 * * *', () => __awaiter(void 0, void 0, void 0, function* () {
-    // se ejecutara a las 23 horas de cada dia
-    try {
-        const db = nano.use(process.env.COUCHDB_NAME ? process.env.COUCHDB_NAME : '');
-        const queryActions = yield db.find({ selector: {
-                couchdb_type: "LOANAPP_GROUP"
-            } });
-        console.log(queryActions.docs.length);
-    }
-    catch (e) {
-        console.log(`NODE-CRON: ${e.message}`);
-    }
-}));
+// import cron from 'node-cron';
+// import * as Nano from 'nano';
+// let nano = Nano.default(`${process.env.COUCHDB_PROTOCOL}://${process.env.COUCHDB_USER}:${process.env.COUCHDB_PASS}@${process.env.COUCHDB_HOST}:${process.env.COUCHDB_PORT}`);
+// cron.schedule('* 23 * * *', async () => {
+//     // se ejecutara a las 23 horas de cada dia
+//     try{
+//         const db = nano.use(process.env.COUCHDB_NAME ? process.env.COUCHDB_NAME : '');
+//         const queryActions = await db.find({ selector: {
+//             couchdb_type: "LOANAPP_GROUP"
+//         }});
+//         console.log(queryActions.docs.length)
+//     }
+//     catch(e:any){
+//         console.log(`NODE-CRON: ${e.message}`);
+//     }
+// });
